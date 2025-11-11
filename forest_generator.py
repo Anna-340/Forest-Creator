eb10546bb9b5e5588c60e9817fe7011c973b2d1f
Annaliese <Annaliesepalm3@gmail.com>
2025-11-11 17:57:17 -0600

    finished adding ipossible imports for this code

--- forest_generator.py
+++ forest_generator.py
@@ -3,11 +3,12 @@ import json
 import random
 import math
 from PySide2 import QtWidgets, QtCore, QtGui
-from PySide2.QtWidgets
-
-
-
-
+from PySide2.QtWidgets import *
+from PySide2.QtCore import *
+from PySide.QtGui import *
+import maya.cmds as cmds
+import maya.mel as mel
+import maya.OpenMaya as om
 
 
 class ForestCreator:
