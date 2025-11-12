16a12bde7cfbe5955a7e50838b16194c93073410
Annaliese <Annaliesepalm3@gmail.com>
2025-11-11 19:22:32 -0600

    adding more pesudo code for creating the forst and finding places to put the objects so they dont overlap

--- forest_generator.py
+++ forest_generator.py
@@ -20,10 +20,24 @@ class ForestCreator:
     # - tab for sapcing and orientation of objects in the scene
     # - tab for colors, user can also pick colors 
     # at the bottom there will be a create forest button and a clear scene option
+    # add status bar at bottom for user
 
 
     #def for creating trees, mushrooms, and rocks using simple geometry,
-    #  but just the bare assets
+    #  but just the bare assets for duplicating later
+    # tree = cylinder for trunk and editted sphere for leaves
+    # rock = editted cube shape
+    # mushroom = cylinder for stem and upsidedown cone for cap
 
-    
+    #def for creating forest, 
+    # duplicate assets for scattering around area from user settings
+    # add random shuffle too
+    # for each of the assets find a place on the plane where they wont overlap
+    # if there is an area that is free copy and place the asset there
+    # organize assets by groups to keep it organized 
+
+    #def for finding a place on the grid where no other asset is placed
+    # to avoid overlapping,
+
+
 
