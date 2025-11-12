cd2a5fead5c2a5d02a136d40305d8903ce7a6d26
Annaliese <Annaliesepalm3@gmail.com>
2025-11-11 18:17:10 -0600

    added pesudocode for ui def and defs for forest assets

--- forest_generator.py
+++ forest_generator.py
@@ -12,3 +12,18 @@ import maya.OpenMaya as om
 
 
 class ForestCreator:
+
+    # ui def
+    # create window for user 
+    #add multiple tabs in window for user
+    # - tab for tree, rock and mushrom generation and amt for both
+    # - tab for sapcing and orientation of objects in the scene
+    # - tab for colors, user can also pick colors 
+    # at the bottom there will be a create forest button and a clear scene option
+
+
+    #def for creating trees, mushrooms, and rocks using simple geometry,
+    #  but just the bare assets
+
+    
+
