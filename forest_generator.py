ccf91dcced137ffbf5956c45d211a5d1170d1b57
Annaliese <Annaliesepalm3@gmail.com>
2025-11-16 20:57:48 -0600

    forgot to add 2 to PySide2 on line 8

--- forest_generator.py
+++ forest_generator.py
@@ -5,7 +5,7 @@ import math
 from PySide2 import QtWidgets, QtCore, QtGui
 from PySide2.QtWidgets import *
 from PySide2.QtCore import *
-from PySide.QtGui import *
+from PySide2.QtGui import *
 import maya.cmds as cmds
 import maya.mel as mel
 import maya.OpenMaya as om
