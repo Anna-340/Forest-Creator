fb2c239706d64ae2d84362e8585fbaa4173527e9
Annaliese <Annaliesepalm3@gmail.com>
2025-11-28 12:07:30 -0600

    deleted completed pesudocode

--- forest_generator.py
+++ forest_generator.py
@@ -13,25 +13,10 @@ import maya.OpenMaya as om
 
 class ForestCreator(QtWidgets.QDialog):
 
-    # ui def
-    # create window for user 
-    #add multiple tabs in window for user
-    # - tab for tree, rock and mushrom generation and amt for both
-    # - tab for sapcing and orientation of objects in the scene
-    # - tab for colors, user can also pick colors 
-    # at the bottom there will be a create forest button and a clear scene option
-    # add status bar at bottom for user
-
      #def for creating forest, 
     # duplicate assets for scattering around area from user settings
     # add random shuffle too
     # for each of the assets find a place on the plane where they wont overlap
-    # if there is an area that is free copy and place the asset there
-    # organize assets by groups to keep it organized 
-
-    #def for finding a place on the grid where no other asset is placed
-    # to avoid overlapping,
-
 
     def __init__(self, parent=None):
         super(ForestCreator, self).__init__(parent)
